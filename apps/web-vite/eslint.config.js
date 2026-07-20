7a19d5c2abc4e2258b90e521d2fd6fb8605791fe
Rafael Santos <rafael.santos@cagtechhub.com>
2026-07-20 14:10:52 +0100

    first commit

---
+++ apps/web-vite/eslint.config.js
@@ -0,0 +1,23 @@
+import { config as baseConfig } from "@repo/eslint-config/base";
+import reactRefresh from "eslint-plugin-react-refresh";
+import reactHooks from "eslint-plugin-react-hooks";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  ...baseConfig,
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+]);
